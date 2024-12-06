b63329b856e2f73f2f34bde60d6b7df60ee3683d
Mannu Kumar Pandey <mannupandeyskit001@gmail.com>
2024-12-06 20:52:42 +0530

    Add .gitignore file changes.

--- src/main/java/priorityqueue/PriorityQueueLRUNature.java
+++ src/main/java/priorityqueue/PriorityQueueLRUNature.java
@@ -19,9 +19,9 @@ public class PriorityQueueLRUNature {
         System.out.println("-------------------------");
 
         pq.remove();
-        //Means Priority Queue stores the deletion duplicate elements as well.
-        // In case of deletion of teh element which has duplicate values stored in priority
-        // queue , It will delete the latest entry of teh same value.
+        /*Means Priority Queue stores the deletion duplicate elements as well.
+         In case of deletion of teh element which has duplicate values stored in priority
+         queue , It will delete the latest entry of teh same value.*/
         for(int i: pq){
             System.out.println(i);
         }
